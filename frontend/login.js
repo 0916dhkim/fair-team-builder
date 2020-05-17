cbc97cd4abbbc2911d7f408bbbfbbb0262d6ba2e
Donghyeon Kim <0916dhkim@gmail.com>
2020-05-17 15:51:57 +0900

    Implement User Verification

--- frontend/login.js
+++ frontend/login.js
@@ -1,10 +1,27 @@
 const {
-    Client
+    Client,
+    AccountInfoQuery
 } = require("@hashgraph/sdk");
 
 const accountIdInput = document.getElementById("accountId");
 const publicKeyInput = document.getElementById("publicKey");
 const privateKeyInput = document.getElementById("privateKey");
+
+async function verifyUser(user) {
+    const client = Client.forTestnet();
+    client.setOperator(
+        user.accountId,
+        user.privateKey
+    );
+    const tx = await new AccountInfoQuery()
+        .setAccountId(user.accountId)
+        .execute(client);
+    if (tx.isDeleted) {
+        throw "Cannot verify deleted account.";
+    }
+
+    return user;
+}
 
 function login(e) {
     e.preventDefault();
@@ -16,10 +33,15 @@ function login(e) {
 
     console.log("Logging in", user);
 
-    localStorage.setItem("user", JSON.stringify(user));
-
-    // Redirect to index.
-    window.location.replace("/");
+    Promise.resolve(verifyUser(user)).then(
+        (user) => {
+            localStorage.setItem("user", JSON.stringify(user));
+            window.location.replace("/");
+        },
+        (error) => {
+            console.error(error);
+        }
+    )
 }
 
 const form = document.getElementById("loginForm");
