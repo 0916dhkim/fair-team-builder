f8dc18511e819c99be912352a2f4db7d6be7734e
Donghyeon Kim <0916dhkim@gmail.com>
2020-05-17 13:44:12 +0900

    Add Hedera Client

--- environment_variables.js
+++ environment_variables.js
@@ -2,6 +2,10 @@
 require("dotenv").config();
 
 // Provide default values.
+// There is no default for operatorId and operatorKey.
 module.exports = {
-    port: process.env.PORT || 3000
+    port: process.env.PORT || 3000,
+    specialChar: process.env.SPECIAL_CHAR || "ℏ",
+    operatorId: process.env.OPERATOR_ID,
+    operatorKey: process.env.OPERATOR_KEY
 };
--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 const express = require("express");
+const {
+    Client
+} = require("@hashgraph/sdk");
 const environment_variables = require("./environment_variables");
+
+// Hedera
+const client = Client.forTestnet();
+client.setOperator(
+    environment_variables.operatorId,
+    environment_variables.operatorKey
+);
+
+// Express
 const app = express();
-
 app.get("/", (req, res) => res.send("Hello, World!"));
-
 app.listen(
     environment_variables.port,
     () => console.log(`Fair Team Builder server listening on port ${environment_variables.port}.`)
